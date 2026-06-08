e71532b4dcf47a762fad7f723e3351dcdaf10c55
hangtime319 <hangtime31@hotmail.com>
2026-06-08 18:01:41 -0300

    Início do projeto

---
+++ docs/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return <h1 className="text-3xl text-center font-bold text-red-800">Hello world!</h1>;
+};
+
+export default App;
