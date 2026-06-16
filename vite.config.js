1d3f43e3116f417b4e78fd8b288d9d36820fec7a
hangtime319 <hangtime31@hotmail.com>
2026-06-16 17:24:16 -0300

    chore: add bundle visualizer and update gitignore

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/results-summary-component/",
+  base: "/result-summary-component-main/",
   plugins: [react(), tailwindcss()],
 });
